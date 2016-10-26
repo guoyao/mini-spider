95d8c9cddfc2427ff61cf581b653b4c0190feb0f
wuguoyao <wuguoyao@baidu.com>
2016-10-27 00:37:59 +0800

    init repo

---
+++ src/mini_spider.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "spider"
+)
+
+func main() {
+	fmt.Println("mini spider...")
+}
---
+++ src/spider/spider.go
@@ -0,0 +1,7 @@
+package spider
+
+import "fmt"
+
+func init() {
+	fmt.Println("package spider init...")
+}
